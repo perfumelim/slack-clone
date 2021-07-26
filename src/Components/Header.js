435eb4e69a8ece6e1a9bfa1440685759426bd2cb
perfumellim <perfumelim@gmail.com>
2021-07-26 22:17:29 +0900

    init project

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <h1>test</h1>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div``;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDBK8w0K69Mymr8RXRuB1IrE_o5eHa6YMA",
+  authDomain: "slack-clone-redux-4c2aa.firebaseapp.com",
+  projectId: "slack-clone-redux-4c2aa",
+  storageBucket: "slack-clone-redux-4c2aa.appspot.com",
+  messagingSenderId: "531390952585",
+  appId: "1:531390952585:web:63fa10e7541f3ebfdb0258",
+  measurementId: "G-G3ETYDRV2D",
+};
