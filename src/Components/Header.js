ae7eb5d9428fa35aa30631c25758e8523bcbfcdd
perfumellim <perfumelim@gmail.com>
2021-07-27 22:50:23 +0900

    [ADD] Header 레이아웃

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 
 function Header() {
   return (
     <HeaderContainer>
-      <h1>test</h1>
+      {/*Header left*/}
+      <HeaderLeft>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+      {/*Header Search*/}
+
+      {/*Header right*/}
     </HeaderContainer>
   );
 }
 
 export default Header;
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margint-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)``;
