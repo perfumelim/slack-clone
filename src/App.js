54366f640201438e386717026d2bea21e4acb09e
perfumellim <perfumelim@gmail.com>
2021-07-28 21:42:22 +0900

    [ADD] Sidebar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* Chat */}
+              </Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -20,3 +26,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
   return (
@@ -12,8 +14,15 @@ function Header() {
         <AccessTimeIcon />
       </HeaderLeft>
       {/*Header Search*/}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search" />
+      </HeaderSearch>
 
       {/*Header right*/}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 }
@@ -22,6 +31,13 @@ export default Header;
 
 const HeaderContainer = styled.div`
   display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
 `;
 
 const HeaderLeft = styled.div`
@@ -31,9 +47,46 @@ const HeaderLeft = styled.div`
   margin-left: 20px;
 
   > .MuiSvgIcon-root {
-    margint-left: auto;
+    margin-left: auto;
     margin-right: 30px;
   }
 `;
 
-const HeaderAvatar = styled(Avatar)``;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  display: flex;
+  flex: 0.4;
+  padding: 0 50px;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
